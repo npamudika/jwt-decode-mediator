bb21c1c179c6975026774df422ec6840a2486386
npamudika <rpnpamudika@gmail.com>
2018-07-07 13:52:11 +0530

    Add JWT Decode Mediator

---
+++ src/main/java/org/wso2/apim/JwtDecodeMediator.java
@@ -0,0 +1,78 @@
+package org.wso2.apim;
+
+import org.apache.synapse.mediators.AbstractMediator;
+import org.apache.synapse.MessageContext;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import org.apache.commons.codec.binary.Base64;
+
+/**
+ * Jwt Decode Mediator Implementation.
+ */
+public class JwtDecodeMediator extends AbstractMediator {
+
+    private static final Logger log = LoggerFactory.getLogger(JwtDecodeMediator.class);
+
+    private String jwtHeader;
+    private String accountIds;
+
+    @Override
+    public boolean mediate(MessageContext mc) {
+        accountIds = retrieveAccountId(getJWT_HEADER());
+        mc.setProperty("accountIds", accountIds);
+        log.info("--------------------ACCOUNT_IDs--------------------" + accountIds);
+        return true;
+    }
+
+    public void setJWT_HEADER(String jwtHeader){
+        this.jwtHeader = jwtHeader;
+    }
+
+    public String getJWT_HEADER(){
+        return jwtHeader;
+    }
+
+    public void setAccountIds(String accountIds){
+        this.accountIds = accountIds;
+    }
+
+    public String getAccountIds(){
+        return accountIds;
+    }
+
+    private static String retrieveAccountId(String accountRequestInfo) {
+        String[] split_string = accountRequestInfo.split("\\.");
+        String base64EncodedBody = split_string[1];
+
+        Base64 base64 = new Base64();
+        try {
+            String decodedString = new String(base64.decode(base64EncodedBody.getBytes()));
+            JSONParser parser = new JSONParser();
+            JSONObject accountRequestInfoJson = (JSONObject) parser.parse(decodedString);
+            if (accountRequestInfoJson.containsKey("accountRequestIds")) {
+                JSONArray accountRequestIdsArray = (JSONArray) accountRequestInfoJson.get("accountRequestIds");
+                String[] accountIdsArray = new String[accountRequestIdsArray.size()];
+                JSONObject accountRequestId;
+                for(int i=0;i<accountRequestIdsArray.size();i++) {
+                    accountRequestId = (JSONObject) accountRequestIdsArray.get(i);
+                    accountIdsArray[i] = accountRequestId.get("accountId").toString();
+                    accountIdsArray[i] = accountIdsArray[i].split("\\[")[1].split("\\]")[0];
+                }
+                String accountIds;
+                accountIds = String.join(",", accountIdsArray);
+                return accountIds;
+            } else {
+                if (log.isDebugEnabled()) log.error("Account Request Ids is not available");
+            }
+        } catch (ParseException e) {
+            log.error("Error in passing Account-Request-Information " + e.toString());
+        }
+        return null;
+    }
+}
